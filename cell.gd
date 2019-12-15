c5aa681ad4e1c84e94c6f9b545b44f47a5cf0ed2
Kai Eckert <hallo@kaiec.de>
2019-12-15 22:37:07 +0100

    all cells pickable

--- cell.gd
+++ cell.gd
@@ -8,14 +8,11 @@ var highlight = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	set_pickable(true)
+	connect("mouse_entered", self, "_on_cell_mouse_entered")
+	connect("mouse_exited", self, "_on_cell_mouse_exited")
 
 export var color = Color(.282, .757, .255)
-
-func _draw():
-	if highlight:
-		draw_circle(size / 2, size.x * 0.42, Color(1,1,1))
-	draw_circle(size / 2, size.x * 0.4, color)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -27,11 +24,11 @@ func set_size(new_size):
 	get_node("CollisionShape2D").set_position(new_size/2)
 	update()
 
-func _on_dot_mouse_entered():
+func _on_cell_mouse_entered():
 	highlight = true
 	update()
 
 
-func _on_dot_mouse_exited():
+func _on_cell_mouse_exited():
 	highlight = false
 	update()
--- game.gd
+++ game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var dot = preload('res://dot.tscn')
+onready var pipe = preload('res://pipe.tscn')
 
 var cell_size = Vector2(100,100)
 var grid_size = Vector2(5,5)
@@ -14,19 +15,21 @@ var colors = {
 	PINK = Color(.914, .118, .388)
 }
 
-var grid = []
+var grid = {}
 
 func _ready():
 	print('Game started')
 	for x in range(grid_size.x):
-		grid.append([])
 		for y in range(grid_size.y):
-			grid[x].append(0)
-	set_dot(Vector2(0,0), colors.GREEN)
-	set_dot(Vector2(4,4), colors.GREEN)
-	set_dot(Vector2(0,4), colors.BLUE)
-	set_dot(Vector2(3,1), colors.BLUE)
-	
+			grid[Vector2(x,y)] = null
+	set_cell(Vector2(0,0), dot, colors.GREEN)
+	set_cell(Vector2(4,4), dot,  colors.GREEN)
+	set_cell(Vector2(0,4), dot,  colors.BLUE)
+	set_cell(Vector2(3,1), dot,  colors.BLUE)
+	for x in range(grid_size.x):
+		for y in range(grid_size.y):
+			if grid[Vector2(x,y)] == null:
+				set_cell(Vector2(x,y), pipe, colors.YELLOW)
 	
 			
 func _draw():
@@ -35,10 +38,11 @@ func _draw():
 			var p = Vector2(x,y)
 			draw_rect(Rect2(grid_pos + p * cell_size, cell_size), colors.WHITE, false) 
 
-func set_dot(pos,color):
-	var newdot = dot.instance()
-	newdot.set_size(cell_size)
-	newdot.set_position(grid_pos + pos * cell_size)
-	newdot.color = color
-	add_child(newdot)
-	return newdot
+func set_cell(pos,cell,color):
+	var newcell = cell.instance()
+	newcell.set_size(cell_size)
+	newcell.set_position(grid_pos + pos * cell_size)
+	newcell.color = color
+	add_child(newcell)
+	grid[pos] = newcell
+	return newcell
