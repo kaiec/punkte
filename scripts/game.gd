b8674e7c68d07800ab4b64dc35ebd926a7a7331f
Kai Eckert <hallo@kaiec.de>
2019-12-19 22:15:01 +0100

    Stripped down for performance tests

--- scripts/game.gd
+++ scripts/game.gd
@@ -28,8 +28,6 @@ func _ready():
 	for x in range(grid_size.x):
 		for y in range(grid_size.y):
 			grid[Vector2(x,y)] = null
-	create_line(Vector2(0,0), Vector2(4,4), colors.GREEN)
-	create_line(Vector2(0,4), Vector2(3,1),  colors.BLUE)
 	for x in range(grid_size.x):
 		for y in range(grid_size.y):
 			if grid[Vector2(x,y)] == null:
@@ -43,22 +41,21 @@ func _ready():
 	InputMap.action_add_event('touch', touchEvent) 
 	InputMap.add_action('drag')
 	InputMap.action_add_event('drag', dragEvent) 
+	
+
 			
 func _draw():
 	for x in range(grid_size.x):
 		for y in range(grid_size.y):
 			var p = Vector2(x,y)
 			draw_rect(Rect2(grid_pos + p * cell_size, cell_size), colors.WHITE, false) 
+	if active_cell:
+		draw_circle(active_cell.position + 0.5 * cell_size, 0.5 * cell_size.x, colors.WHITE)
 
 func game_pos(pos):
 	var gp_float = ((pos - grid_pos) / cell_size)
 	var gp = gp_float.floor()
-	var excenter = (gp_float - gp - Vector2(0.5, 0.5)).length_squared()
-	# print('%s, %s, %s' % [gp_float, gp, excenter])
-	if excenter > 0.16:
-		return null
-	else:
-		return gp
+	return gp
 	
 	
 func _input(event):
@@ -69,29 +66,16 @@ func _input(event):
 			if gp != null and grid[gp] != active_cell:
 				if not active_cell and not active_line:
 					active_cell = grid[gp]
-					grid[gp].try_start_pipe()
+					update()
 				else:
 					active_cell = grid[gp]
-					grid[gp].try_connect_to_pipe()
+					update()
 	elif event is InputEventScreenTouch and not event.pressed:
 		if active_line:
 			active_line.end()
 		active_cell = null	
 
 
-
-func create_line(pos1, pos2, color):
-	var line = Line.new()
-	line.game = self
-	var dot1 = set_cell(pos1, Dot, color)
-	var dot2 = set_cell(pos2, Dot, color)
-	dot1.init_line(line)
-	dot2.init_line(line)
-	dot1.other = dot2
-	dot2.other = dot1
-	line.dots.append(dot1)
-	line.dots.append(dot2)
-	
 
 func set_cell(pos,cell,color):
 	var newcell = cell.instance()
@@ -104,13 +88,3 @@ func set_cell(pos,cell,color):
 	grid[pos] = newcell
 	return newcell
 	
-	
-func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.pressed and event.scancode == KEY_R:
-			print('refresh')
-			for cell in grid.values():
-				cell.update()
-		if event.pressed and event.scancode == KEY_D:
-			print_debug('click on a cell to dump it')
-			dumpcell = true
