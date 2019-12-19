6434035b9aec7ced82fb99b7ecd684ba8376a19b
Kai Eckert <hallo@kaiec.de>
2019-12-19 22:44:35 +0100

    Pull

--- scripts/game.gd
+++ scripts/game.gd
@@ -9,10 +9,13 @@ func _ready():
 var pos = Vector2(0,0)
 			
 func _draw():
-		draw_circle(pos, 15, Color(1,1,1))
+		draw_circle(get_local_mouse_position(), 15, Color(1,1,1))
 
+	
+func _physics_process(delta):
+	update()
 	
 func _input(event):
 	if event is InputEventScreenDrag:
 		position = event.position
-		update()
+
