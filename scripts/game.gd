669c7eeafb1c270e78f19307f02e372eaa53523d
Kai Eckert <hallo@kaiec.de>
2019-12-27 14:18:50 +0100

    Further tests with reduced framerate

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,12 +7,18 @@ func _ready():
 
 
 var pos = Vector2(0,0)
+var time := 0.0
 			
 func _draw():
 		draw_circle(pos, 15, Color(1,1,1))
 
 	
+func _physics_process(delta):
+	time += delta
+	if time > 0.1:
+		update()
+		time = 0
+	
 func _input(event):
 	if event is InputEventScreenDrag:
-		position = event.position
-		update()
+		pos = event.position
